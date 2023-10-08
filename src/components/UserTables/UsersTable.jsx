048651f7aabf6aac77027ef5c6c1aa9847e990e6
mrAnderson90 <tyler.andreev@mail.ru>
2023-10-09 00:02:10 +0300

    Add sorting to UserTable.jsx

--- src/components/UserTables/UsersTable.jsx
+++ src/components/UserTables/UsersTable.jsx
@@ -1,33 +1,40 @@
-import React from "react";
+import React from 'react';
+import * as _ from 'lodash';
+
+const { useState } = React;
 
 export default function UsersTable({ users }) {
-    users.forEach((user)=> {
-        console.log(user)
-    })
+    const [userList, setUserList ] = useState(users);
+
+		const sort = (coll, value) => _.sortBy(coll, (user) => user[value]);
+
+		const handleSort = (e) => {
+			const { id } = e.target;
+			const sortedUserList = sort(userList, id);
+			setUserList(sortedUserList);
+		};
+
+		const tableStyle = {
+			'display': 'grid',
+			'gridTemplateColumns': 'repeat(5, 10%)',
+		};
+
     return (
-        <div>
-           <table>
-						<thead>
-							<tr>
-								<th>ID</th>
-								<th>Name</th>
-								<th>Surname</th>
-								<th>Birthday</th>
-								<th>Email</th>
-							</tr>
-						</thead>
-						<tbody>
-							{users.map((user) => (
-								<tr key={user.id}>
-									<td>{user.id}</td>
-									<td>{user.name}</td>
-									<td>{user.surname}</td>
-									<td>{user.birthData.toLocaleDateString('ru-RU')}</td>
-									<td>{user.email}</td>
-								</tr>
-							))}
-						</tbody>
-					 </table>
+        <div style={tableStyle}>
+           <div id="id" onClick={handleSort}>ID</div>
+           <div id="name" onClick={handleSort}>Name</div>
+           <div id="surname" onClick={handleSort}>Surname</div>
+           <div id="birthData" onClick={handleSort}>Birthday</div>
+           <div id="email" onClick={handleSort}>Email</div>
+					 {userList.map(({ id, name, surname, birthData, email }) => (
+						<React.Fragment key={id}>
+							<div>{id}</div>
+							<div>{name}</div>
+							<div>{surname}</div>
+							<div>{birthData.toLocaleDateString('ru-RU')}</div>
+							<div>{email}</div>
+						</React.Fragment>
+					 ))}
         </div>
     );
 }
