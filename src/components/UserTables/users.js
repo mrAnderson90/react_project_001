1822b1c6d8d8f6b3f1988ff6246076cd5a93682a
mrAnderson90 <tyler.andreev@mail.ru>
2023-10-09 22:15:49 +0300

    Add users

--- src/components/UserTables/users.js
+++ src/components/UserTables/users.js
@@ -34,4 +34,75 @@ export const users = [
         birthData: new Date('1998-04-03'),
         email: "john5@email.com",
         id: 5
+    },
+    // new users
+    {
+      name: "John",
+      surname: "Snow",
+      birthData: new Date('1998-04-03'),
+      email: "night.watcher@stark.com",
+      id: 6
+    },
+    {
+      name: "Jamie",
+      surname: "Lannister",
+      birthData: new Date('1998-04-03'),
+      email: "great.knight@lannister.com",
+      id: 7
+    },
+    {
+      name: "Margery",
+      surname: "Tirrell",
+      birthData: new Date('1998-04-03'),
+      email: "margo8@tirrell.com",
+      id: 8
+    },
+    {
+      name: "Teon",
+      surname: "Greyjoy",
+      birthData: new Date('1998-04-03'),
+      email: "teon.stinky@nodead.com",
+      id: 9
+    },
+    {
+      name: "Sansa",
+      surname: "Stark",
+      birthData: new Date('1998-04-03'),
+      email: "sansa10@stark.com",
+      id: 10
+    },
+    {
+      name: "Daeneryes",
+      surname: "Targaryen",
+      birthData: new Date('1998-04-03'),
+      email: "denny.myssa@targaryen.com",
+      id: 11
+    },
+    {
+      name: "Serseya",
+      surname: "Lannister",
+      birthData: new Date('1998-04-03'),
+      email: "bloody.queen@lannister.com",
+      id: 12
+    },
+    {
+      name: "Arya",
+      surname: "Stark",
+      birthData: new Date('1998-04-03'),
+      email: "noface.girl@stark.com",
+      id: 13
+    },
+    {
+      name: "Tirion",
+      surname: "Lannister",
+      birthData: new Date('1998-04-03'),
+      email: "righthand.forever@lannister.com",
+      id: 14
+    },
+    {
+      name: "Oberin",
+      surname: "Martell",
+      birthData: new Date('1998-04-03'),
+      email: "lover.boy@sand.com",
+      id: 15
     }]
